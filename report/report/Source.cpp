65d8b7490a3d70535a32aafc9ec798ed89396ef8
Ho <dog536536@naver.com>
2016-09-21 15:24:16 +0900

    Complete

---
+++ report/report/Source.cpp
@@ -0,0 +1,163 @@
+#include <GLFW/glfw3.h>
+#include <algorithm>
+
+const int width = 640;
+const int height = 480;
+
+float* pixels = new float[width*height * 3];
+
+void drawPixel(const int& i, const int& j, const float& red, const float& green, const float& blue)
+{
+	pixels[(i + width* j) * 3 + 0] = red;
+	pixels[(i + width* j) * 3 + 1] = green;
+	pixels[(i + width* j) * 3 + 2] = blue;
+}
+
+void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
+{
+	for (int i = i0; i <= i1; i++)
+	{
+		const int j = (j1 - j0)*(i - i0) / (i1 - i0) + j0;
+
+		drawPixel(i, j, red, green, blue);
+	}
+}
+
+void drawBox(const int& i_c, const int& j_c)
+{
+	const int thickness = 60;   //µÎ²²
+
+	for (int j = j_c - thickness; j < j_c + thickness; j++)
+		for (int i = i_c - thickness; i < i_c + thickness; i++)
+		{
+			drawPixel(i, j, 0.0f, 1.0f, 0.0f);
+		}
+}
+
+void drawTriangle(const int& left, const int& right, const int& up)
+{
+	drawLine(left, left, right, left, 1.0f, 0.0f, 1.0f);
+	drawLine(up, up, right, left, 1.0f, 0.0f, 1.0f);
+	drawLine(left, left, up, up, 1.0f, 0.0f, 1.0f);
+}
+
+void drawEmptyBox(const int& i_s, const int& j_s, const int& i_e, const int& j_e)  //s=start, e=end
+{
+	for (int j = j_s; j <= j_e; j++)
+		for (int i = i_s; i <= i_e; i++)
+		{
+			if (j == j_e || j == j_s || i == i_s || i == i_e)
+				drawPixel(i, j, 1.0f, 0.0f, 0.0f);
+			else
+				drawPixel(i, j, 1.0f, 1.0f, 1.0f);
+		}
+}
+
+//void drawPentagon(const int& i0, const int& i1, const int& i2, const int& i3, const int& i4)  //No
+//{
+//	drawLine(i0, i0, i1, i1, 0.0f, 0.0f, 1.0f);
+//	drawLine(i1, i1, i2, i2, 0.0f, 0.0f, 1.0f);
+//	drawLine(i2, i2, i3, i3, 0.0f, 0.0f, 1.0f);
+//	drawLine(i3, i3, i4, i4, 0.0f, 0.0f, 1.0f);
+//	drawLine(i4, i4, i0, i0, 0.0f, 0.0f, 1.0f);
+//}
+
+void drawCircle(const int& x_c, const int& y_c, const int& radius)  //first - distance=radius ->draw
+{
+	for (double j = 0; j<height; j++)
+		for (double i = 0; i < width; i++)
+		{
+			double func = ((i - x_c)*(i - x_c) + (j - y_c)*(j - y_c) - radius*radius);
+
+			if (func >= 0 && func <= 60)    //func>=0&&func<=9 , func == 0||func==1||func==2||func==5
+				drawPixel(i, j, 1.0f, 0.0f, 0.0f);
+			else
+				drawPixel(0, 0, 1.0f, 1.0f, 1.0f);
+		}
+}
+
+//void drawCircle(const int& x_c, const int y_c)
+//{
+//	const double x_c = 0.5;
+//	const double y_c = 0.5;
+//	const double r = 0.5;
+//
+//	const double func = (x - x_c)*(x - x_c) + (y - y_c)*(y - y_c) - r*r;
+//
+//	if (func > 0.0) return false;
+//	else return true;
+//}
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(width, height, "GLFW Test", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		std::fill_n(pixels, width*height * 3, 1.0f);	// white background
+
+
+		drawLine(50, 350, 150, 450, 0.0f, 0.0f, 1.0f);
+		drawLine(51, 350, 151, 450, 0.0f, 0.0f, 1.0f);
+		drawLine(52, 350, 152, 450, 0.0f, 0.0f, 1.0f);  //3-width-line
+
+														//for(int j=100;j<300;j++)
+														//	for (int i = 100; i < 300; i++)
+														//	{
+														//		pixels[(i + width*j) * 3 + 0] = 0.0f;
+														//		pixels[(i + width*j) * 3 + 1] = 1.0f;
+														//		pixels[(i + width*j) * 3 + 2] = 0.0f;
+														//	}
+
+		drawBox(550, 400);               //full box
+
+		drawEmptyBox(250, 300, 400, 450);   //i_s,j_s,i_e,j_e, empty box
+
+		drawTriangle(40, 160, 120);     //left,right,up  triangle
+
+										//drawLine(50, 150, 130, 229, 0.0f, 0.0f, 1.0f);
+										//drawLine(51, 150, 180, 150, 0.0f, 0.0f, 1.0f);
+										//drawLine(130, 229, 180, 150, 0.0f, 0.0f, 1.0f);  //triangle
+
+		drawLine(300, 100, 370, 50, 0.0f, 0.0f, 1.0f);
+		drawLine(230, 50, 300, 100, 0.0f, 0.0f, 1.0f);
+		drawLine(230, 50, 260, 10, 0.0f, 0.0f, 1.0f);
+		drawLine(260, 10, 340, 10, 0.0f, 0.0f, 1.0f);
+		drawLine(340, 10, 370, 50, 0.0f, 0.0f, 1.0f);   //pentagon
+
+		drawCircle(500, 100, 40);  //circle
+
+		glDrawPixels(width, height, GL_RGB, GL_FLOAT, pixels);
+		//We do that.
+
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	delete[] pixels;
+
+	glfwTerminate();
+	return 0;
+}
